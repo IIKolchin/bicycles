e2793221938f52ad89fe06e43241a51b4e751d54
Ivan Kolchin <kolchin.ivan@mail.ru>
2021-10-03 01:56:40 +0300

    сверстать блок с велосипедами

--- scripts/script.js
+++ scripts/script.js
@@ -3,4 +3,53 @@ new Swiper(".slider__container", {
     nextEl: ".slider__button-next",
     prevEl: ".slider__button-prev",
   },
+
+  loop: true,
+});
+
+let appendNumber = 3;
+let prependNumber = 1;
+const swiper = new Swiper(".bicycles__slider");
+
+document
+  .querySelector(".bicycle__button_highway")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.slideTo(0, 0);
+  });
+
+document
+  .querySelector(".bicycle__button_grevel")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.slideTo(1, 0);
+  });
+
+document
+  .querySelector(".bicycle__button_tt")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.slideTo(2, 0);
+  });
+
+const buttonHighway = document.querySelector(".bicycle__button_highway");
+const buttonGrevel = document.querySelector(".bicycle__button_grevel");
+const buttonTt = document.querySelector(".bicycle__button_tt");
+
+buttonHighway.addEventListener("click", function () {
+  buttonHighway.classList.add("bicycle__button-active");
+  buttonGrevel.classList.remove("bicycle__button-active");
+  buttonTt.classList.remove("bicycle__button-active");
+});
+
+buttonGrevel.addEventListener("click", function () {
+  buttonGrevel.classList.add("bicycle__button-active");
+  buttonHighway.classList.remove("bicycle__button-active");
+  buttonTt.classList.remove("bicycle__button-active");
+});
+
+buttonTt.addEventListener("click", function () {
+  buttonTt.classList.add("bicycle__button-active");
+  buttonHighway.classList.remove("bicycle__button-active");
+  buttonGrevel.classList.remove("bicycle__button-active");
 });
